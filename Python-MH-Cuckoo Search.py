ef40b2f0a55d267886cb14745ab594458b3e6027
Valdecy Pereira <valdecypereira@yahoo.com.br>
2019-08-16 11:16:38 -0300

    Update Python-MH-Cuckoo Search.py

--- Python-MH-Cuckoo Search.py
+++ Python-MH-Cuckoo Search.py
@@ -12,20 +12,22 @@
 ############################################################################
 
 # Required Libraries
-import pandas as pd
 import numpy  as np
 import math
 import random
 import os
 
+# Function
+def target_function():
+    return
+
 # Function: Initialize Variables
-def initial_position(birds = 3, min_values = [-5,-5], max_values = [5,5]):
-    position = pd.DataFrame(np.zeros((birds, len(min_values))))
-    position['Fitness'] = 0.0
+def initial_position(birds = 3, min_values = [-5,-5], max_values = [5,5], target_function = target_function):
+    position = np.zeros((birds, len(min_values) + 1))
     for i in range(0, birds):
         for j in range(0, len(min_values)):
-             position.iloc[i,j] = random.uniform(min_values[j], max_values[j])
-        position.iloc[i,-1] = target_function(position.iloc[i,0:position.shape[1]-1])
+             position[i,j] = random.uniform(min_values[j], max_values[j])
+        position[i,-1] = target_function(position[i,0:position.shape[1]-1])
     return position
 
 # Function: Levy Distribution
@@ -35,74 +37,68 @@ def levy_flight(mean):
 	return x1*x2
 
 # Function: Replace Bird
-def replace_bird(position, alpha_value = 0.01, lambda_value = 1.5, min_values = [-5,-5], max_values = [5,5]):
-    random_bird = np.random.randint(position.shape[0], size = 1)[0]
-    new_solution = pd.DataFrame(np.zeros((1, position.shape[1])))
-        
+def replace_bird(position, alpha_value = 0.01, lambda_value = 1.5, min_values = [-5,-5], max_values = [5,5], target_function = target_function):
+    random_bird  = np.random.randint(position.shape[0], size = 1)[0]
+    new_solution = np.zeros((1, position.shape[1]))  
     for j in range(0, position.shape[1] - 1):
-        new_solution.iloc[0, j] = position.iloc[random_bird, j] + alpha_value*levy_flight(lambda_value)*position.iloc[random_bird, j]*(int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1))
-        if (new_solution.iloc[0,j] > max_values[j]):
-            new_solution.iloc[0,j] = max_values[j]
-        elif (new_solution.iloc[0,j] < min_values[j]):
-            new_solution.iloc[0,j] = min_values[j] 
-    new_solution.iloc[0,-1] = target_function(new_solution.iloc[0,0:new_solution.shape[1]-1])
-    
-    if (position.iloc[random_bird, -1] > new_solution.iloc[0,-1]):
-        for j in range(0, position.shape[1]):
-            position.iloc[random_bird, j] = new_solution.iloc[0, j]
+        new_solution[0, j] = np.clip(position[random_bird, j] + alpha_value*levy_flight(lambda_value)*position[random_bird, j]*(int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)), min_values[j], max_values[j])
+    new_solution[0,-1] = target_function(new_solution[0,0:new_solution.shape[1]-1])
+    if (position[random_bird, -1] > new_solution[0,-1]):
+        position[random_bird, j] = np.copy(new_solution[0, j])
     return position
 
 # Function: Update Positions
-def update_positions(position, discovery_rate = 0.25, min_values = [-5,-5], max_values = [5,5]):
-    updated_position = position.copy(deep = True)
-    abandoned_nests = math.ceil(discovery_rate*updated_position.shape[0]) + 1
-    random_bird_j = np.random.randint(position.shape[0], size = 1)[0]
-    random_bird_k = np.random.randint(position.shape[0], size = 1)[0]
+def update_positions(position, discovery_rate = 0.25, min_values = [-5,-5], max_values = [5,5], target_function = target_function):
+    updated_position = np.copy(position)
+    abandoned_nests  = math.ceil(discovery_rate*updated_position.shape[0]) + 1
+    random_bird_j    = np.random.randint(position.shape[0], size = 1)[0]
+    random_bird_k    = np.random.randint(position.shape[0], size = 1)[0]
     while(random_bird_j == random_bird_k):
         random_bird_j = np.random.randint(position.shape[0], size = 1)[0]
-    nest_list = list(position.nlargest(abandoned_nests - 1, "Fitness").index.values)
-   
+    nest_list = list(position.argsort()[-(abandoned_nests-1):][::-1][0])
     for i in range(0, updated_position.shape[0]):
         for j in range(0, len(nest_list)):
             rand = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
             if(i == nest_list[j] and rand > discovery_rate):
                 for k in range(0, updated_position.shape[1] - 1):
                     rand = int.from_bytes(os.urandom(8), byteorder = "big") / ((1 << 64) - 1)
-                    updated_position.iloc[i,k] = updated_position.iloc[i,k]  + rand*(updated_position.iloc[random_bird_j,k] - updated_position.iloc[random_bird_k,k])
-                    if (updated_position.iloc[i,k] > max_values[k]):
-                        updated_position.iloc[i,k] = max_values[k]
-                    elif (updated_position.iloc[i,k] < min_values[k]):
-                        updated_position.iloc[i,k] = min_values[k] 
-        updated_position.iloc[i,-1] = target_function(updated_position.iloc[i,0:updated_position.shape[1]-1])
-                
+                    updated_position[i,k] = np.clip(updated_position[i,k]  + rand*(updated_position[random_bird_j,k] - updated_position[random_bird_k,k]), min_values[k], max_values[k])
+        updated_position[i,-1] = target_function(updated_position[i,0:updated_position.shape[1]-1])          
     return updated_position
 
 # CS Function
-def cuckoo_search(birds = 3, discovery_rate = 0.25, alpha_value = 0.01, lambda_value = 1.5, min_values = [-5,-5], max_values = [5,5], iterations = 50):    
+def cuckoo_search(birds = 3, discovery_rate = 0.25, alpha_value = 0.01, lambda_value = 1.5, min_values = [-5,-5], max_values = [5,5], iterations = 50, target_function = target_function):    
     count = 0
-    position = initial_position(birds = birds, min_values = min_values, max_values = max_values)    
-    best_ind = position.iloc[position['Fitness'].idxmin(),:].copy(deep = True)
-    
+    position = initial_position(birds = birds, min_values = min_values, max_values = max_values, target_function = target_function)    
+    best_ind = np.copy(position[position[:,-1].argsort()][0,:])
     while (count <= iterations):
-        print("Iteration = ", count, " of ", iterations, " f(x) = ", best_ind[-1])
-        
+        print("Iteration = ", count, " of ", iterations, " f(x) = ", best_ind[-1])    
         for i in range(0, position.shape[0]):
-            position = replace_bird(position, alpha_value = alpha_value, lambda_value = lambda_value, min_values = min_values, max_values = max_values)
-        position = update_positions(position, discovery_rate = discovery_rate, min_values = min_values, max_values = max_values)
-
-        if (best_ind[-1] > position.iloc[position['Fitness'].idxmin(),:][-1]):
-            best_ind = position.iloc[position['Fitness'].idxmin(),:].copy(deep = True)  
-            
+            position = replace_bird(position, alpha_value = alpha_value, lambda_value = lambda_value, min_values = min_values, max_values = max_values, target_function = target_function)
+        position = update_positions(position, discovery_rate = discovery_rate, min_values = min_values, max_values = max_values, target_function = target_function)
+        value = np.copy(position[position[:,-1].argsort()][0,:]) 
+        if (best_ind[-1] > value[-1]):
+            best_ind = np.copy(position[position[:,-1].argsort()][0,:])     
         count = count + 1 
-        
     print(best_ind)    
     return best_ind
 
 ######################## Part 1 - Usage ####################################
 
-# Function to be Minimized. Solution ->  f(x1, x2) = -1.0316; x1 = 0.0898, x2 = -0.7126 or x1 = -0.0898, x2 = 0.7126
-def target_function (variables_values = [0, 0]):
+# Function to be Minimized (Six Hump Camel Back). Solution ->  f(x1, x2) = -1.0316; x1 = 0.0898, x2 = -0.7126 or x1 = -0.0898, x2 = 0.7126
+def six_hump_camel_back(variables_values = [0, 0]):
     func_value = 4*variables_values[0]**2 - 2.1*variables_values[0]**4 + (1/3)*variables_values[0]**6 + variables_values[0]*variables_values[1] - 4*variables_values[1]**2 + 4*variables_values[1]**4
     return func_value
 
-cs = cuckoo_search(birds = 50, discovery_rate = 0.25, alpha_value = 0.01, lambda_value = 1.5, min_values = [-5,-5], max_values = [5,5], iterations = 1000)
+cs = cuckoo_search(birds = 500, discovery_rate = 0.25, alpha_value = 0.01, lambda_value = 1.5, min_values = [-5,-5], max_values = [5,5], iterations = 1000, target_function = six_hump_camel_back)
+
+# Function to be Minimized (Rosenbrocks Valley). Solution ->  f(x) = 0; xi = 1
+def rosenbrocks_valley(variables_values = [0,0]):
+    func_value = 0
+    last_x = variables_values[0]
+    for i in range(1, len(variables_values)):
+        func_value = func_value + (100 * math.pow((variables_values[i] - math.pow(last_x, 2)), 2)) + math.pow(1 - last_x, 2)
+    return func_value
+
+cs = cuckoo_search(birds = 1000, discovery_rate = 0.02, alpha_value = 0.15, lambda_value = 1.5, min_values = [-5,-5], max_values = [5,5], iterations = 100, target_function = rosenbrocks_valley)
+
